8898deb557100d23d0f0a4299c8584e38244e522
Jo Wagner <jowagner@limago.de>
2023-09-11 08:04:21 +0200

    initial commit

---
+++ interface-segregation-gui/src/de/limago/Main.java
@@ -0,0 +1,32 @@
+package de.limago;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main extends Frame {
+
+
+    public Main()  {
+
+
+        setSize(300, 300);
+        Button button = new Button("Drück mich");
+
+        add(button);
+    }
+
+
+
+    public static void main(String[] args) {
+
+        new Main().setVisible(true);
+    }
+
+   
+
+
+
+
+
+
+}
